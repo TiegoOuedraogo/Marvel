e12e27288c95c9a30a78be47e809e2f9bc107c52
Tiego Ouedraogo <tiegoouedraogo@gmail.com>
2024-02-13 18:53:26 -0500

    Header implemented with the Links

---
+++ src/components/Header.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+
+const Header = () => {
+
+    return (
+        <header>
+            <vav>
+                <ul>
+                    <li><a href="/news">NEWS</a></li>
+                    <li><a href="/comics">COMICS</a></li>
+                    <li><a href="/characters">CHARACTERS</a></li>
+                    <li><a href="/movies">MOVIES</a></li>
+                    <li><a href="/tv-shows">TV SHOWS</a></li>
+                    <li><a href="/games">GAMES</a></li>
+                    <li><a href="/videos">VIDEOS</a></li>
+                    <li><a href="/more">MORE</a></li>
+                </ul>
+            </vav>
+        </header>
+    )
+
+}
+
+export default Header;
